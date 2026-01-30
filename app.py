fc3ac3071f3edbe4befa11166dd9dd7dfc4fa660
codychennn <girlmixboy@gmail.com>
2026-01-30 08:58:01 +0800

    Initialize Flask app with home route

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
+from flask import Flask, render_template
 
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    # 這裡未來會串接機票 API
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
